0a5e271fe8520fea2b587d419547ed6d9968c717
GugaSantos <guga.santos.guga@gmail.com>
2022-07-30 00:14:17 -0300

    Feat: Criada as validações para as pessoas usuárias

---
+++ src/middlewares/userErrors.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Request, Response } from 'express';
+import Joi from 'joi';
+
+export function UserErrors(req:Request, res:Response, next:NextFunction) {
+  const { username, classe, level, password } = req.body;
+  if (!username) return res.status(400).json({ message: '"username" is required' });
+  if (!classe) return res.status(400).json({ message: '"classe" is required' });
+  if (!level && level !== 0) return res.status(400).json({ message: '"level" is required' });
+  if (!password) return res.status(400).json({ message: '"password" is required' });
+  next();
+}
+
+export function UserJoiValidation(req:Request, res:Response, next:NextFunction) {
+  const schema = Joi.object({
+    username: Joi.string().min(3).required(),
+    classe: Joi.string().min(3).required(),
+    level: Joi.number().min(1).required(),
+    password: Joi.string().min(8).required(),
+  });
+
+  const validate = schema.validate(req.body);
+
+  if (validate.error) return res.status(422).json({ message: validate.error.message });
+
+  next();
+}
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import UserController from '../controllers/users.controller';
+import { UserErrors, UserJoiValidation } from '../middlewares/userErrors';
 
 const userController = new UserController();
 
 const router = Router();
 
-router.post('/users', userController.create);
+router.post('/users', UserErrors, UserJoiValidation, userController.create);
 
 export default router;
